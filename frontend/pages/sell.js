6ff0dfda572672ce43170a50644149cd9b00d845
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2021-02-06 22:54:28 +1030

    Creating a react form and useForm custom hook

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,3 +1,9 @@
+import CreateProduct from '../components/CreateProduct';
+
 export default function SellPage() {
-        return <p>Hello I am the sell page! Test</p>;
+        return (
+                <div>
+                        <CreateProduct />
+                </div>
+        );
 }
