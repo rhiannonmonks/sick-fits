95d41617bdbd858ede914cddb22d05d213d6d54b
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2021-02-07 17:52:30 +1030

    creating a signout component

--- frontend/components/SignIn.js
+++ frontend/components/SignIn.js
@@ -1,4 +1,5 @@
-import { gql, useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
 import Form from './styles/Form';
 import useForm from '../lib/useForm';
 import { CURRENT_USER_QUERY } from './User';
@@ -14,7 +15,7 @@ const SIGNIN_MUTATION = gql`
                                         name
                                 }
                         }
-                        ... on UserAuthenticationWithPasswordSuccess {
+                        ... on UserAuthenticationWithPasswordFailure {
                                 code
                                 message
                         }
@@ -29,31 +30,32 @@ export default function SignIn() {
         });
         const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
                 variables: inputs,
-                // refetch the currently logged in user
+                // refectch the currently logged in user
                 refetchQueries: [{ query: CURRENT_USER_QUERY }],
         });
         async function handleSubmit(e) {
                 e.preventDefault(); // stop the form from submitting
                 console.log(inputs);
-                const res = await Signin();
+                const res = await signin();
                 console.log(res);
-                // send the email and password to the graphQL API
+                resetForm();
+                // Send the email and password to the graphqlAPI
         }
         const error =
-                data?.authenticatedUserWithPassword.__typename === 'UserAuthenticatedWithPasswordFailure'
-                        ? data?.authenticatedUserWithPassword
+                data?.authenticateUserWithPassword.__typename === 'UserAuthenticationWithPasswordFailure'
+                        ? data?.authenticateUserWithPassword
                         : undefined;
         return (
-                <Form method="POST">
-                        <h2> Sign In To Your Account</h2>
+                <Form method="POST" onSubmit={handleSubmit}>
+                        <h2>Sign Into Your Account</h2>
                         <Error error={error} />
                         <fieldset>
                                 <label htmlFor="email">
                                         Email
                                         <input
                                                 type="email"
-                                                name="name"
-                                                placeholder="Your email address"
+                                                name="email"
+                                                placeholder="Your Email Address"
                                                 autoComplete="email"
                                                 value={inputs.email}
                                                 onChange={handleChange}
@@ -63,14 +65,14 @@ export default function SignIn() {
                                         Password
                                         <input
                                                 type="password"
-                                                password="password"
-                                                placeholder="Your password"
+                                                name="password"
+                                                placeholder="Password"
                                                 autoComplete="password"
                                                 value={inputs.password}
                                                 onChange={handleChange}
                                         />
                                 </label>
-                                <button type="submit">Sign in!</button>
+                                <button type="submit">Sign In!</button>
                         </fieldset>
                 </Form>
         );
