28d4df46c4a38d623da19dba95f3c3cf1dd44523
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2021-02-07 22:28:23 +1030

    adding cart state

--- backend/.keystone/admin/pages/_app.js
+++ backend/.keystone/admin/pages/_app.js
@@ -198,7 +198,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="d7olix"
+        adminMetaHash="lk4zc9"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
