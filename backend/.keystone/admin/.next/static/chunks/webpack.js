53bd18a160802d9c26837e0a63bbc889ae146917
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2021-02-08 17:28:31 +1030

    linked frontend to the custom backend checkout mutation

--- backend/.keystone/admin/.next/static/chunks/webpack.js
+++ backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6c2efd468c3865aebee5";
+/******/ 	var hotCurrentHash = "cb6e3d4f32cdeb3a72cd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Checkout.js
+++ frontend/components/Checkout.js
@@ -3,6 +3,8 @@ import { loadStripe } from '@stripe/stripe-js';
 import { CardElement, Elements, useElements, useStripe } from '@stripe/react-stripe-js';
 import { useState } from 'react';
 import nProgress from 'nprogress';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
 import SickButton from './styles/SickButton';
 
 const CheckoutFormStyles = styled.form`
@@ -14,6 +16,20 @@ const CheckoutFormStyles = styled.form`
         grid-gap: 1rem;
 `;
 
+const CREATE_ORDER_MUTATION = gql`
+        mutation CREATE_ORDER_MUTATION($token: String!) {
+                checkout(token: $token) {
+                        id
+                        charge
+                        total
+                        items {
+                                id
+                                name
+                        }
+                }
+        }
+`;
+
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
 function CheckoutForm() {
@@ -21,29 +37,38 @@ function CheckoutForm() {
         const [loading, setLoading] = useState(false);
         const stripe = useStripe();
         const elements = useElements();
+        const [checkout, { error: graphQLError }] = useMutation(CREATE_ORDER_MUTATION);
         async function handleSubmit(e) {
-                // stop the form from submitting and turn the loader on
-
+                // 1. Stop the form from submitting and turn the loader one
                 e.preventDefault();
                 setLoading(true);
                 console.log('We gotta do some work..');
-                // start the page transtion
+                // 2. Start the page transition
                 nProgress.start();
-                // create the payment method via stripe
-                // Token comes back if successful
+                // 3. Create the payment method via stripe (Token comes back here if successful)
                 const { error, paymentMethod } = await stripe.createPaymentMethod({
                         type: 'card',
                         card: elements.getElement(CardElement),
                 });
                 console.log(paymentMethod);
-                // Handle any errors from Stripe
+                // 4. Handle any errors from stripe
                 if (error) {
                         setError(error);
+                        nProgress.done();
+                        return; // stops the checkout from happening
                 }
-                // Send the token to our keystone server via a custom mutation
-                // Change the page to view the order
-                // Close the cart
-                // turn the loader off
+                // 5. Send the token from step 3 to our keystone server, via a custom mutation!
+                const order = await checkout({
+                        variables: {
+                                token: paymentMethod.id,
+                        },
+                });
+                console.log(`Finished with the order!!`);
+                console.log(order);
+                // 6. Change the page to view the order
+                // 7. Close the cart
+
+                // 8. turn the loader off
                 setLoading(false);
                 nProgress.done();
         }
@@ -51,6 +76,7 @@ function CheckoutForm() {
         return (
                 <CheckoutFormStyles onSubmit={handleSubmit}>
                         {error && <p style={{ fontSize: 12 }}>{error.message}</p>}
+                        {graphQLError && <p style={{ fontSize: 12 }}>{graphQLError.message}</p>}
                         <CardElement />
                         <SickButton>Check Out Now</SickButton>
                 </CheckoutFormStyles>
