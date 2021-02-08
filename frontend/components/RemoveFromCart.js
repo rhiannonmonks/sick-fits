494bce68c2db9a2ca94cce0c3553a75d2004ae8e
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2021-02-08 13:47:47 +1030

    evicting cart items from the cache

---
+++ frontend/components/RemoveFromCart.js
@@ -0,0 +1,43 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+
+const BigButton = styled.button`
+font-size: 3rem;
+background: none;
+border 0;
+&:hover {
+    color: font-variant-numeric(--red);
+cursor: pointer;
+}
+`;
+
+function update(cache, payload) {
+        cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
+const REMOVE_FROM_CART_MUTATION = gql`
+        mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
+                deleteCartItem(id: $id) {
+                        id
+                }
+        }
+`;
+
+export default function RemoveFromCart({ id }) {
+        const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
+                variables: { id },
+                update,
+                // optimisticResponse: {
+                //         deleteCartItem: {
+                //                 __typename: 'CartItem',
+                //                 id,
+                //         },
+                // },
+        });
+        return (
+                <BigButton onClick={removeFromCart} disabled={loading} type="button" title="Remove this Item from Cart">
+                        &times;
+                </BigButton>
+        );
+}
